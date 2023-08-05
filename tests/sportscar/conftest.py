b2556e75c9a5158b544d27a8d259b5183410112f
ozy <giuseppe.fur96@gmail.com>
2023-08-05 12:11:55 +0200

    changes fixture scope to session and adds comments

--- tests/sportscar/conftest.py
+++ tests/sportscar/conftest.py
@@ -1,7 +1,11 @@
+# Only test in the same directory or deeper can access to it and NO need to import it
+
 from pytest import fixture
 from selenium import webdriver
 
-@fixture(scope='function')
+
+# scope of the fixture can be session (i.e. one browser per session), function (one browser per function/test) and module
+@fixture(scope='session')
 def chrome_browser():
     browser = webdriver.Chrome()
     return browser
